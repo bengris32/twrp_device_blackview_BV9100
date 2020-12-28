e41dbf39f50e4e1d064155323bce9920aa7a69c5
itsjustben <bengris32@protonmail.ch>
2020-12-28 15:08:14 +0000

    bv9100: finally fix encryption * move hals to sbin and symlink them to correct location * add dm_use_original_path to data in fstab (requires patch) * requires patch to support keymaster 2

--- omni_BV9100.mk
+++ omni_BV9100.mk
@@ -15,13 +15,14 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := BV9100
 PRODUCT_NAME := omni_BV9100
-PRODUCT_BRAND := blackview
+PRODUCT_BRAND := Blackview
 PRODUCT_MODEL := BV9100
 PRODUCT_MANUFACTURER := Blackview
 
